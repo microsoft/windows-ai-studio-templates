cb5d128da5c8353924b31c469e6f1657ddf8ef27
Vicente Rivera <vriveras@gmail.com>
2024-04-28 22:41:15 -0700

    feat:Adding Phi3 initial template for local fine-tuning

---
+++ configs/Phi-3-mini-4k-instruct/finetuning/invoke_olive.py
@@ -0,0 +1,8 @@
+from olive.workflows import run as olive_run
+import os
+import wandb
+
+wandb.login()
+# Run olive from file for debug.
+file_path = os.path.join(os.getcwd(), 'finetuning/olive-config.json')
+olive_run(file_path)
