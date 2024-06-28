98780a65464d405b1785275e5c80da3392444edd
Katrina Cruz <kacruz@microsoft.com>
2024-06-28 09:45:13 -0700

    mistral-7b

--- configs/mistral-7b/inference/chat.py
+++ configs/mistral-7b/inference/chat.py
@@ -18,7 +18,7 @@ class ChatBot:
         # Only initializes the model if it has not already been initialized.
         if ChatBot.m is None or ChatBot.tok is None:
             # Define the model name and retrieve the latest model checkpoint.
-            model_name = "../model-cache/mistralai/Mistral-7B"
+            model_name = "<model-download-location>"
             adapters_name = "../models/qlora/qlora/gpu-cpu_model/adapter"
 
             # Logging the model loading process.
