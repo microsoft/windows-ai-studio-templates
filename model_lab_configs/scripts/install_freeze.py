bf275796c26a4ff395796a9e0ac8e6252b5ce1d7
xieofxie <xieofxie@126.com>
2025-05-15 14:19:16 +0800

    Hualxie/intel wcr (#278)
    * update olive version
    
    * update py
    
    * all use new olive
    
    ---------
    
    Co-authored-by: hualxie <hualxie@microsoft.com>

--- model_lab_configs/scripts/install_freeze.py
+++ model_lab_configs/scripts/install_freeze.py
@@ -15,6 +15,8 @@ from model_lab import RuntimeEnum
 def get_requires(name, args):
     if "#egg=" in name:
         package_name = name.split("#egg=")[1]
+    elif name.startswith("./"):
+        package_name = name[2:].split("-")[0].replace("_", "-")
     else:
         package_name = name.split('==')[0]  # Remove version if present
     if "[" in package_name:
@@ -33,23 +35,23 @@ def get_requires(name, args):
 
 def main():
     # Constants
+    # if from git: "git+https://github.com/microsoft/Olive.git@COMMIT_ID#egg=olive_ai
+    oliveAi = "git+https://github.com/microsoft/Olive.git@7e666230bf692a9794abb878f178c2c8be0bb6a9#egg=olive_ai"
+    torchVision = "torchvision==0.22.0"
     pre = {
         RuntimeEnum.NvidiaGPU: [
             "--extra-index-url https://download.pytorch.org/whl/cu126",
             "torch==2.6.0+cu126",
         ],
         RuntimeEnum.AMDNPU: [
-            "numpy==1.26.4"
+            "numpy==1.26.4",
         ],
         RuntimeEnum.IntelNPU: [
-            "torch==2.6.0"
+            "torch==2.6.0",
         ],
-        RuntimeEnum.WCR: [
-            "git+https://github.com/microsoft/Olive.git@377e233de4814b1bc92e173d6dbb503f1f94dc04#egg=olive_ai"
-        ]
     }
     shared = [
-        "olive-ai==0.9.0",
+        oliveAi,
         "tabulate==0.9.0",
         "datasets==3.5.0",
         "ipykernel==6.29.5",
@@ -58,12 +60,12 @@ def main():
     # torchvision, onnxruntime and genai go here. others should go feature
     post = {
         RuntimeEnum.CPU: [
-            "torchvision==0.22.0",
+            torchVision,
             "onnxruntime==1.21.0",
             "onnxruntime-genai==0.7.0"
         ],
         RuntimeEnum.QNN: [
-            "torchvision==0.22.0",
+            torchVision,
             "onnxruntime-qnn==1.21.1",
             "# uvpip:install onnxruntime-genai==0.7.0 --no-deps;post"
         ],
@@ -77,7 +79,7 @@ def main():
             "openvino==2025.1.0",
             "nncf==2.16.0",
             "optimum[openvino]==1.24.0",
-            # optimum-intel==1.15.0: onnxruntime so we need to uninstall first
+            # optimum-intel==1.15.0: depends on onnxruntime so we need to uninstall first
             #"# uvpip:uninstall onnxruntime;post",
             # uninstall first to fix incomplete installation issue
             #"# uvpip:uninstall onnxruntime-openvino;post",
@@ -86,7 +88,7 @@ def main():
             "onnxruntime-genai==0.7.0"
         ],
         RuntimeEnum.AMDNPU: [
-            "torchvision==0.22.0",
+            torchVision,
             "# onnxruntime",
             "./voe-1.5.0.dev20250501191909+g87eb429ad-py3-none-any.whl",
             "./onnxruntime_vitisai-1.22.0.dev20250501-cp310-cp310-win_amd64.whl",
@@ -100,7 +102,7 @@ def main():
             "onnxruntime-genai-cuda==0.7.0"
         ],
         RuntimeEnum.WCR: [
-            "torchvision==0.22.0",
+            torchVision,
             "./onnxruntime_winml-1.22.0-cp312-cp312-win_amd64.whl",
             "./onnxruntime_genai_winml-0.9.0.dev0-cp312-cp312-win_amd64.whl"
         ],
@@ -159,7 +161,7 @@ def main():
     outputFile = path.join(path.dirname(__file__), "..", "docs", f"requirements-{args.runtime}.txt")
     with open(outputFile, "w") as f:
         for name in all:
-            if name.startswith("#") or name.startswith("--") or name.startswith("./"):
+            if name.startswith("#") or name.startswith("--"):
                 f.write(name + "\n")
                 continue
             f.write("# " + name + "\n")
