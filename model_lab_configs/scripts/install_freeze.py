ec74baf42eb26e97c9e9ee974bef571bae2a173c
Chao Zhang <zhangchao@microsoft.com>
2025-07-09 17:14:19 +0800

    fix install_freeze

--- model_lab_configs/scripts/install_freeze.py
+++ model_lab_configs/scripts/install_freeze.py
@@ -61,13 +61,6 @@ def main():
         RuntimeEnum.IntelNPU: [
             "torch==2.6.0",
         ],
-        RuntimeEnum.WCR_CUDA: [
-            "torchvision==0.22.0+cu128",
-            "# uvpip:install onnxruntime-winml==1.22.0 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
-            "# uvpip:install onnxruntime-genai-winml==0.8.0 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
-            "evaluate==0.4.3",
-            "scikit-learn==1.6.1",
-        ],
     }
     shared = [
         # sticking to ONNX IR version 10 which can still be consumed by ORT v1.22.0
@@ -107,6 +100,13 @@ def main():
         RuntimeEnum.WCR: [
             torchVision,
             "# uvpip:install onnxruntime-winml==1.22.0.post1 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
+            "# uvpip:install onnxruntime-genai-winml==0.8.0 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
+            "evaluate==0.4.3",
+            "scikit-learn==1.6.1",
+        ],
+        RuntimeEnum.WCR_CUDA: [
+            "torchvision==0.22.0+cu128",
+            "# uvpip:install onnxruntime-winml==1.22.0 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
             "# uvpip:install onnxruntime-genai-winml==0.8.0 --extra-index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ORT-Nightly/pypi/simple --no-deps;post",
             "evaluate==0.4.3",
             "scikit-learn==1.6.1",
