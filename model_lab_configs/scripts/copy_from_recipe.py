4f53d55c111ce973b85125d3a20ff3eb9efc993e
hualxie <hualxie@microsoft.com>
2025-08-05 13:05:52 +0800

    remove deps

--- model_lab_configs/scripts/copy_from_recipe.py
+++ model_lab_configs/scripts/copy_from_recipe.py
@@ -1,31 +1,37 @@
 import shutil
 from pathlib import Path
+import json
 
-from sanitize.model_info import ModelList
+ignore_patterns = ["info.yml", "_copy.json.config"]
 
+def copy_folder(model, models_dir: Path, olive_recipes_dir: Path):
+    id = model.get("id")
+    version = model.get("version")
+    relative_path = model.get("relativePath")
+    # make dir
+    target_dir = models_dir / Path(id) / Path(str(version))
+    target_dir.mkdir(parents=True, exist_ok=True)
+    source_dir = olive_recipes_dir / Path(relative_path)
+    # copy dir
+    shutil.copytree(source_dir, target_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns(*ignore_patterns))
 
 def main():
     root_dir = Path(__file__).parent.parent.parent
     olive_recipes_dir = root_dir.parent / "olive-recipes"
-    configs_dir = olive_recipes_dir / ".aitk" / "configs"
+    olive_configs_dir = olive_recipes_dir / ".aitk" / "configs"
+    olive_list = olive_configs_dir / "model_list.json"
     models_dir = root_dir / "model_lab_configs"
-    list = ModelList.Read(str(configs_dir))
 
-    # Define files/patterns to ignore
-    ignore_patterns = ["info.yml", "_copy.json.config"]
+    with open(olive_list, "r") as f:
+        list = json.load(f)
 
-    for model in list.allModels():
-        if not model.relativePath:
-            continue
-        # make dir
-        target_dir = models_dir / Path(model.id) / Path(str(model.version))
-        target_dir.mkdir(parents=True, exist_ok=True)
-        source_dir = olive_recipes_dir / Path(model.relativePath)
-        # copy dir
-        shutil.copytree(source_dir, target_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns(*ignore_patterns))
+    for model in list["models"]:
+        copy_folder(model, models_dir, olive_recipes_dir)
+    for model in list["template_models"]:
+        copy_folder(model, models_dir, olive_recipes_dir)    
 
     shutil.copyfile(
-        configs_dir / "model_list.json",
+        olive_list,
         models_dir / "model_list.json",
     )
 
