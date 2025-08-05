c80018f8b941fe9003d9931fe88a48a3bf863408
xieofxie <xieofxie@126.com>
2025-08-05 14:00:55 +0800

    feat: update copy_from_recipe (#361)
    * format inference_model.json
    
    * add copy_from_recipe
    
    * update copy
    
    * nit
    
    * nit
    
    * remove deps
    
    * test
    
    * inside
    
    * nit
    
    ---------
    
    Co-authored-by: hualxie <hualxie@microsoft.com>

--- model_lab_configs/scripts/copy_from_recipe.py
+++ model_lab_configs/scripts/copy_from_recipe.py
@@ -1,33 +1,62 @@
+import json
 import shutil
+import subprocess
 from pathlib import Path
 
-from sanitize.model_info import ModelList
+ignore_patterns = ["info.yml", "_copy.json.config"]
+copied_folders = 0
+
+
+def copy_folder(model, models_dir: Path, olive_recipes_dir: Path):
+    id = model.get("id")
+    version = model.get("version")
+    relative_path = model.get("relativePath")
+    # make dir
+    target_dir = models_dir / Path(id) / Path(str(version))
+    target_dir.mkdir(parents=True, exist_ok=True)
+    source_dir = olive_recipes_dir / Path(relative_path)
+    # copy dir
+    print(f"Copying from {source_dir} to {target_dir}")
+    shutil.copytree(source_dir, target_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns(*ignore_patterns))
+    global copied_folders
+    copied_folders += 1
+
+
+def save_commit_id(models_dir: Path, olive_recipes_dir: Path):
+    result = subprocess.run(
+        ["git", "rev-parse", "HEAD"], cwd=olive_recipes_dir, capture_output=True, text=True, check=True
+    )
+    commit_id = result.stdout.strip()
+    with open(models_dir / "commit_id.txt", "w") as f:
+        f.write(commit_id)
 
 
 def main():
     root_dir = Path(__file__).parent.parent.parent
     olive_recipes_dir = root_dir.parent / "olive-recipes"
-    configs_dir = olive_recipes_dir / ".aitk" / "configs"
+    if not olive_recipes_dir.exists():
+        olive_recipes_dir = root_dir / "olive-recipes"
+        if not olive_recipes_dir.exists():
+            raise FileNotFoundError("olive-recipes directory not found.")
+    olive_configs_dir = olive_recipes_dir / ".aitk" / "configs"
+    olive_list = olive_configs_dir / "model_list.json"
     models_dir = root_dir / "model_lab_configs"
-    list = ModelList.Read(str(configs_dir))
 
-    # Define files/patterns to ignore
-    ignore_patterns = ["info.yml", "_copy.json.config"]
+    with open(olive_list, "r") as f:
+        list = json.load(f)
 
-    for model in list.allModels():
-        if not model.relativePath:
-            continue
-        # make dir
-        target_dir = models_dir / Path(model.id) / Path(str(model.version))
-        target_dir.mkdir(parents=True, exist_ok=True)
-        source_dir = olive_recipes_dir / Path(model.relativePath)
-        # copy dir
-        shutil.copytree(source_dir, target_dir, dirs_exist_ok=True, ignore=shutil.ignore_patterns(*ignore_patterns))
+    for model in list["models"]:
+        copy_folder(model, models_dir, olive_recipes_dir)
+    for model in list["template_models"]:
+        copy_folder(model, models_dir, olive_recipes_dir)
 
     shutil.copyfile(
-        configs_dir / "model_list.json",
+        olive_list,
         models_dir / "model_list.json",
     )
+    save_commit_id(models_dir, olive_recipes_dir)
+    global copied_folders
+    print(f"Copied {copied_folders} folders from olive-recipes to model_lab_configs.")
 
 
 if __name__ == "__main__":
